7b6766f4827b5e7c9851ab4ee830d8b31fb3850b
darm96 <diego.ruiz@civico.com>
2016-11-11 11:02:49 -0500

    add fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User
   include Mongoid::Document
+
+  # Campos
   field :_id, type: String
   field :_slugs, type: String
+  field :firstname, type: String
+  field :lastname, type: String
+  field :attached_provider, :type => Hash, :default => {}
+
+
 end
