9b97251e12088aed303da1a838c526cac00d6aa2
darm96 <diego.ruiz@civico.com>
2016-11-11 09:09:59 -0500

    Rails 3.2.21 no active record

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Productiva::Application.config.secret_token = '1413a427199346e9a0cc3e9039f2b1b580b1d457c78dc293e874298a326b9927a4e8f4210c7c0077402fb4c918cb5cd24f3f0ff2f947d5c3c3a9463ba40e392c'
